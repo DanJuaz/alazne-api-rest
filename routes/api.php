23ba0fdd03a92275e9b85b119cd6b78aa00e1acf
Julio <jazurduymontalvo@gmail.com>
2025-06-16 10:17:55 +0200

    New routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DatabaseController;
+use App\Http\Controllers\Api\ReservaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,17 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Database connection check
-Route::get('/check-connection', [\App\Http\Controllers\Api\DatabaseController::class, 'checkConnection']);
+// Public routes
+Route::post('login', [AuthController::class, 'login']);
 
-// Login
-Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+// Database public routes
+Route::get('check-connection', [DatabaseController::class, 'checkConnection']);
+Route::get('available-bookings', [DatabaseController::class, 'availableBookings']);
+Route::post('bookings', [DatabaseController::class, 'postBooking']);
 
-// CRUD Reservas
-Route::apiResource('resevation', \App\Http\Controllers\Api\ReservaController::class);
-
-// Database available bookings
-Route::get('/available-bookings', [\App\Http\Controllers\Api\DatabaseController::class, 'availableBookings']);
-
-// Database post booking
-Route::post('/booking', [\App\Http\Controllers\Api\DatabaseController::class, 'postBooking']);
+// Protected routes
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'me']);
+    
+    // CRUD Reservas
+    Route::apiResource('reservas', ReservaController::class);
+});
