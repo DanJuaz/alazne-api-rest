55c709e131680e4f0d556d1a866d6e5a90f57625
Julio <jazurduymontalvo@gmail.com>
2025-06-16 10:18:18 +0200

    Table Usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,13 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model
+class Usuario extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
+
     protected $table = 'usuarios';
 
     protected $fillable = [
-        'usuario', 'password', 'nivel', 'nombre', 'estado', 'status', 'token'
+        'usuario',
+        'password',
+        'nivel',
+        'nombre',
+        'estado',
+        'status'
     ];
+
+    protected $hidden = [
+        'password',
+        'token',
+    ];
+
+    protected $casts = [
+        'nivel' => 'integer',
+        'estado' => 'integer',
+        'status' => 'integer',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'usuario' => $this->usuario,
+            'nivel' => $this->nivel,
+            'nombre' => $this->nombre
+        ];
+    }
 }
